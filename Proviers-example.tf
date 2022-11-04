2ee37b5ba333b1235f72bb23874fe393e0718559
Djafar Laouedj <djafar@grow-una.com>
2022-11-04 10:36:54 +0100

    Add providers & sensitive-data examples

---
+++ Proviers-example.tf
@@ -0,0 +1,27 @@
+#####################################################
+## AZURE PROVIDER
+#####################################################
+
+/* terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.29.1"
+    }
+
+    # Random Provider
+    random = {
+      source  = "hashicorp/random"
+      version = "= 3.4.3"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "xxxx"
+  client_id       = "xxxx"
+  client_secret   = "xxxx"
+  tenant_id       = "xxxx"
+}
+ */
---
+++ sensitive-data-example.tf
@@ -0,0 +1,11 @@
+#####################################################
+## Tenant ID
+#####################################################
+
+/* variable "var_tenant_id" {
+  default = "xxx"
+}
+
+variable "var_sp_terraform" {
+  default = "xxxx"
+} */
